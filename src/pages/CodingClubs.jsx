0512527641c389021c0bc5dbaa9163cd472713c7
Isaac <isaacfunes@icloud.com>
2024-01-25 13:32:04 -0600

    Placed placeholders within pages header

--- src/pages/CodingClubs.jsx
+++ src/pages/CodingClubs.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const CodingClubs = () => {
   return (
-    <div>CodingClubs</div>
+    <div className='robomania__header section__padding' id='home'>
+      <div className="robomania__header-content">
+        <h1 className="gradient__text">Coding Clubs</h1>
+        <p>Where young minds ignite their passion for STEM through the exciting world of robotics and coding! Dive into a journey of discovery and innovation, as we offer hands-on experiences that transform curiosity into real-world skills, nurturing creativity, critical thinking, and a lifelong love for learning in the dynamic fields of science and technology.</p>
+        
+        <div className="robomania__header-content__input">
+          <button type='button'>About Us</button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
