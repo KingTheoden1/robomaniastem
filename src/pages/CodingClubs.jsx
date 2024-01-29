cf17f337026b008bade09eddf365d866c11ce543
Isaac <isaacfunes@icloud.com>
2024-01-28 23:22:34 -0600

    All but 1 bug fixed on page

--- src/pages/CodingClubs.jsx
+++ src/pages/CodingClubs.jsx
@@ -15,25 +15,31 @@ const CodingClubs = () => {
       </div> {/* Close the header div here */}
 
       {/* Separate New Sections */}
-      <div className="new-sections-container">
-        <div className="program-section">
-          <div className="text-content">
-            <h2 className='gradient__text'>New Students</h2>
-            <p>Explore the wonderful world of Python programming. Learn how to design interactive stories and games using Python programming. Understand the fundamentals computer science (CS) concepts using Python. Design engaging characters and exciting game levels and improve computational thinking (CT) skills.<br/><br/>Tuition: $129<br/><br/>Session duration: One month</p>
-          </div>
-          <img src={IntroToPython} alt="Description of Image One" />
+      <div className="programs">
+      <h2 className="gradient__text programs-title">Coding Clubs</h2>
+      <div className="program-item">
+        <div>
+          <h3 className='gradient__text'>New Students</h3>
+          <p>Explore the wonderful world of Python programming. Learn how to design interactive stories and games using Python programming. Understand the fundamentals computer science (CS) concepts using Python. Design engaging characters and exciting game levels and improve computational thinking (CT) skills.<br/><br/>Tuition: $129<br/><br/>Session duration: One month</p>
+          <button onClick={() => window.location.href = '/coding-clubs'}>Learn More</button>
         </div>
+        <img src={IntroToPython} alt="New Students" />
+      </div>
 
-        <div className="program-section">
-          <div className="text-content">
-            <h2 className='gradient__text'>Continuing Students</h2>
-            <p>After completing the Introduction to Python session students will continue thier coding journey with Python in session 2 - 4. During session 2 - 4, students continue to build Computational thinking (CT) with a focus on Computer Science (CS) skills and problem solving. <br/><br/>Students will Learn advance coding skills in Python, a leading industry standard computer programming language. Students will engage in month-long problems solving projects and exercises which will encourage learning of new skills.<br/><br/>Tuition: $129<br/><br/>Session duration: One month (4 classes per month)</p>
-          </div>
-          <img src={ComputerProgramming} alt="Description of Image Two" />
+      <div className="program-item alternate">
+        <img src={ComputerProgramming} alt="Continuing Students" />
+        <div>
+          <h3 className='gradient__text'>Continuing Students</h3>
+          <p>After completing the Introduction to Python session students will continue thier coding journey with Python in session 2 - 4. During session 2 - 4, students continue to build Computational thinking (CT) with a focus on Computer Science (CS) skills and problem solving. <br/><br/>Students will Learn advance coding skills in Python, a leading industry standard computer programming language. Students will engage in month-long problems solving projects and exercises which will encourage learning of new skills.<br/><br/>Tuition: $129<br/><br/>Session duration: One month (4 classes per month)</p>
+          <button onClick={() => window.location.href = '/coding-clubs'}>Learn More</button>
         </div>
       </div>
+    </div>
     </>
   );
 }
 
 export default CodingClubs;
+
+// Explore the wonderful world of Python programming. Learn how to design interactive stories and games using Python programming. Understand the fundamentals computer science (CS) concepts using Python. Design engaging characters and exciting game levels and improve computational thinking (CT) skills.<br/><br/>Tuition: $129<br/><br/>Session duration: One month
+// After completing the Introduction to Python session students will continue thier coding journey with Python in session 2 - 4. During session 2 - 4, students continue to build Computational thinking (CT) with a focus on Computer Science (CS) skills and problem solving. <br/><br/>Students will Learn advance coding skills in Python, a leading industry standard computer programming language. Students will engage in month-long problems solving projects and exercises which will encourage learning of new skills.<br/><br/>Tuition: $129<br/><br/>Session duration: One month (4 classes per month)
