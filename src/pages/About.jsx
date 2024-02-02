67ae5c50efd568e7bdcc44c9d62e6ce5de975972
Isaac <isaacfunes@icloud.com>
2024-02-01 19:46:06 -0600

    Fixed image sizing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './About.css'; // Ensure this CSS file exists and is correctly linked
-import IntroToPython from './../assets/IntroToPython.jpg'
-import ComputerProgramming from './../assets/ComputerProgramming.jpg'
 import CTSkills from './../assets/ctskills.jpg'
 
 const CodingClubs = () => {
@@ -28,10 +26,12 @@ const CodingClubs = () => {
         <div>
           <h3 className='gradient__text'>Computational Thinking (CT) Skills</h3>
           <p>There are certain concepts, practices, and perspectives that are fundamemtal to becoming an effective coder. These fundamental skills are called Computational thining or CT skills. Research suggests that students who are able to master CT skills at an early age are more likely to succeed in demanding STEM courses beyond computer science at secondary and post-seocndary levels​(1).<br /><br />The map of CT skills below shows how Data is at the center of the CT concepts which are surrounded by CT practices that foster CT perspectives. This CT map was designed by Dr. Aamir Fidai based on the research done by Brannan & Resnick at MIT Media Lab in 2012(2)​.</p>
-          <img className='img__ct' src={CTSkills} alt="CT Skills" />
-          <p>Research References:<br /><br />1. Ye, J., Lai, X., & Wong, G. K. W. (2022). The transfer effects of computational thinking: A systematic review with meta‐analysis and qualitative synthesis. Journal of Computer Assisted Learning, 38(6), 1620-1638.<br /><br />​2. Brennan, K., & Resnick, M. (2012, April). New frameworks for studying and assessing the development of computational thinking. In Proceedings of the 2012 annual meeting of the American educational research association, Vancouver, Canada (Vol. 1, p. 25).</p>
+          
+          
         </div>
       </div>
+      <img className='about__image' src={CTSkills} alt="CT Skills" />
+      <p>Research References:<br /><br />1. Ye, J., Lai, X., & Wong, G. K. W. (2022). The transfer effects of computational thinking: A systematic review with meta‐analysis <br />and qualitative synthesis. Journal of Computer Assisted Learning, 38(6), 1620-1638.<br /><br />​2. Brennan, K., & Resnick, M. (2012, April). New frameworks for studying and assessing the development of computational thinking. <br />In Proceedings of the 2012 annual meeting of the American educational research association, Vancouver, Canada (Vol. 1, p. 25).</p>
     </div>
     </>
   );
